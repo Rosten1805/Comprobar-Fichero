c9759b44d868715ff9fc135a6371d2c1c1e9062f
Cristina Cañadas Gómez - Limón <rosten1805@protonmail.com>
2023-03-11 19:49:55 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string ruta = "C:/campions.txt";
+        string himno = "Añadir letra de tu equipo favorito";
+
+
+        if (File.Exists(ruta))
+        {
+            Console.WriteLine("El Himno ha sido añadido");
+        }
+        else
+        {
+            // Crear el archivo y añadir el himno
+            File.WriteAllText(ruta, himno);
+            Console.WriteLine("Archivo creado y himno añadido.");
+        }
+
+        Console.ReadLine();
+    }
+}
